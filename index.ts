013dee9be2b7011ec9dcef3918697aa842a93715
immatte <matte_isabelle@yahoo.fr>
2023-08-17 14:43:53 +0200

    Functions annotations

--- index.ts
+++ index.ts
@@ -26,6 +26,18 @@ const logTodo = (id: number, title: string, completed: boolean) => {
   `);
 };
 
+// Function
+const logNumber:(i:number) => void = (i: number) => {
+  console.log(i);
+}
+
+// When to use annotations
+
+// 1) Function that returns the 'any' type
+const json = '{"x":10, "y":20}';
+const coordinates: { x:number; y:number } = JSON.parse(json);
+console.log(coordinates); //{x:10, y:20}
+
 
 // 2) when declare a variable on one line
 // and initializa it later
